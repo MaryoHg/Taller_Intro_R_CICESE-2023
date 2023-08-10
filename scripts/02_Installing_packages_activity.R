7c2aa0af4d431f0479a0a0dc23f09a6cacd438e3
MaryoHg <hg.maryo@gmail.com>
2023-08-09 22:09:48 -0700

    Actualizado script 02

--- scripts/02_Installing_packages_activity.R
+++ scripts/02_Installing_packages_activity.R
@@ -1,21 +1,20 @@
+## ------------------------------------- ##
 ## Instalar paqueterías en mi ambiente R ##
-##---------------------------------------##
-.libPaths()
+##-------------------------------------- ##
+.libPaths() # coloca aquí tu ruta: 
 
 
-## Instalar paqueterías desde CRAN
+## 1. Instalar paqueterías desde el repositorio CRAN: 
 
-	?install.package()
-	install.packages(c('RColorBrewer', 'Rmisc', 'tidyverse', 'devtools'), dep = T)
+	?install.packages()
+	install.packages(c('RColorBrewer', 'Rmisc', 'tidyverse', 'devtools', 'readxl', 'xlsx'), dependencies = TRUE)
 
-
-## Instalar paqueterías desde Github
+## 2. Instalar paqueterías desde Github
 	## Con devtools:: package
-	
 	if (!requireNamespace("devtools", quietly = TRUE)){install.packages("devtools")}
 	devtools::install_github("jbisanz/qiime2R")
 
-## Con la paquetería remote::
+## 3. Con la paquetería remote::
 	## Note: remotes:: package is a lightweight version of devtools function install_github() function
 	## Note 2: They both do the same exactly
 
@@ -23,16 +22,24 @@
 	remotes::install_github("nrennie/ggflowchart")
 
 
-## Using Bioconductor: at <https://www.bioconductor.org/install/>
+## 4. Instalando desde el repositorio Bioconductor: at <https://www.bioconductor.org/install/>
 	## installing BiocManager package()
-	
 	if (!require("BiocManager", quietly = TRUE))
-		install.packages("BiocManager")
-	# BiocManager::install(version = "3.17")
+		install.packages("BiocManager") # BiocManager::install(version = "3.17")
 	
 	## Package install with BiocManager() - an example
-
 	BiocManager::install("ComplexHeatmap")	# paquete para hacer heatmaps complejos
 	BiocManager::install("ALDEx2")		# paquete para análisis composicional de datos (secuenciamiento masivo).
 	
 	
+## ----------------------------------- ##
+## Cargar paqueterías en mi ambiente R ##
+##------------------------------------ ##
+	# Cargamos una librería para que R pueda disponer de sus funciones.
+        # Si una librería no esta "cargada", corremos el riesgo de no encontrar la función deseada.
+	
+	library(tidyverse)
+	library(qiime2R)
+	library(RColorBrewer)
+	library(devtools)
+	library(ComplexHeatmap)
